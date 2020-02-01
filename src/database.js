173170e90769d2c7d61158e931ff15d9b22dfa18
alejo1026 <dahdhenao@gmail.com>
2020-02-01 15:16:14 -0500

    5commit

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const {mongodb} = require('./keys');
 
 require ('dotenv').config({path: 'src/variables.env'});
-mongoose.connect(process.env.apsaDB ||mongodb.uri,mongodb.options).then (
+mongoose.connect(process.env.apsaDB,mongodb.options).then (
     ()=> {console.log('base de datos conectada');},
     err => {err}
 );
