917e32abd1abbbe29643934f1d4a7a6e5df3c506
alejo1026 <dahdhenao@gmail.com>
2020-02-01 15:51:17 -0500

    6commit

--- src/app/views/InicioSesion.vue
+++ src/app/views/InicioSesion.vue
@@ -86,6 +86,7 @@ export default {
             this.$router.push("/creditos");
         },
         err => {
+          alert('error')
           alert(err.response.data.error);
         }
       );
