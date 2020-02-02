c9ff7df9995fc6d6cd5ce2cf5a1a2b0efce4d813
alejo1026 <dahdhenao@gmail.com>
2020-02-01 22:03:26 -0500

    7commit

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ require ('dotenv').config({path: 'src/variables.env'});
 mongoose.connect(process.env.DB_URL,mongodb.options).then (
     ()=> {console.log('base de datos conectada');},
     err => {
-        console.log('sss')
+        console.log('ssssss')
         console.log(err)
     }
 );
