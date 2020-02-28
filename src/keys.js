9a0d8437f47fbadb9790c75b2740b55cc89f6da0
alejito871026 <31643825+alejito871026@users.noreply.github.com>
2020-02-28 11:24:49 -0500

    Delete keys.js

--- src/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongodb:{
-        uri :'mongodb+srv://apsa:871026@apsa-fgfgw.gcp.mongodb.net/test?retryWrites=true&w=majority',
-        options:{
-            useNewUrlParser:true, useCreateIndex: true, useUnifiedTopology: true
-        }   
-    }
-};
